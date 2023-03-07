0b5b4d0f5a8afa08e9be3eee1b2feaefd212487e
saeedygujjer <119007431+saeedygujjer@users.noreply.github.com>
2023-03-07 13:15:37 +0200

    Update amazon_apstag.user.js

--- amazon_apstag.user.js
+++ amazon_apstag.user.js
@@ -1,26 +1,13 @@
-/*******************************************************************************
-
-    uBlock Origin - a browser extension to block requests.
-    Copyright (C) 2019-present Raymond Hill
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see {http://www.gnu.org/licenses/}.
-
-    Home: https://github.com/gorhill/uBlock
-*/
-
-// https://www.reddit.com/r/uBlockOrigin/comments/ghjqph/
-// https://github.com/NanoMeow/QuickReports/issues/3717
+// ==UserScript==
+// @name         kdkskd_Adblock Script by Saeed
+// @namespace    http://lkubuntu.wordpress.com/
+// @version      1.1
+// @description  adblocker
+// @author       Saeed Akbar
+// @grant       GM_getValue
+// @run-at      document-start
+// @match       * 
+// @include     *
 
 (function() {
     'use strict';
