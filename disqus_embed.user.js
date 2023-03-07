c84a26a79024a1783b138b52b593e8913370c882
saeedygujjer <119007431+saeedygujjer@users.noreply.github.com>
2023-03-07 13:21:59 +0200

    Update disqus_embed.user.js

--- disqus_embed.user.js
+++ disqus_embed.user.js
@@ -1,23 +1,13 @@
-6// ==UserScript==
+// ==UserScript==
 // @name         discusAdblock Script by Saeed
-// @namespace    *
-// @version      1.1
-// @description  adblocker
+// @namespace    saeed
+// @version      17382.1
+// @description  adblockerjs
 // @author       Saeed Akbar
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @grant       GM_registerMenuCommand
-// @run-at      document-start
-// @grant       GM.getResourceUrl
-// @grant       GM_getResourceURL
-// @grant       GM_addStyle
-// @grant       GM.xmlHttpRequest
-// @grant       GM_xmlhttpRequest
-// @grant       GM.openInTab
-// @grant       GM_openInTab
-// @grant       GM_registerMenuCommand
-// @match       * 
-// @include     *
+// @match        * 
+// @include      *
+// @grant        GM_getValue
+// @grant        none
 
 (function() {
     'use strict';
